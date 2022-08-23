bdb1da42e139334631ba004b40d6c69c747ec445
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-08-23 17:30:01 -0300

    Parseo completado

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+from parsear import parsear_archivo
+
+
+def lavados_con(tiempos, incompatibilidades):
+    pass
+
+
+def main():
+    # Parsear archivo
+    tiempos, incompatibilidades = parsear_archivo()
+
+    # Algoritmo
+    lavados = lavados_con(tiempos, incompatibilidades)
+
+    # Salida
+
+
+if __name__ == '__main__':
+    main()
---
+++ parsear.py
@@ -0,0 +1,45 @@
+def parsear_archivo():
+    tiempos = {}
+    incompatibilidades = {}
+    f = open("primer_problema.txt", "r")
+    for linea in f.readlines():
+        linea = linea.strip()
+        comando, *args = linea.split(" ")
+        dic[comando](args, tiempos, incompatibilidades)
+
+    return tiempos, incompatibilidades
+
+
+def comando_c(args, tiempos={}, incompatibilidades={}):
+    pass
+
+
+def comando_p(args, tiempos={}, incompatibilidades={}):
+    pass
+
+
+def comando_e(args, _tiempos, incompatibilidades: Dict[object, list]):
+    prenda1, prenda2 = args
+    if prenda1 not in incompatibilidades:
+        incompatibilidades.setdefault(prenda1, [])
+    if prenda2 not in incompatibilidades:
+        incompatibilidades.setdefault(prenda2, [])
+
+    incompatibilidades[prenda1].append(prenda2)
+    incompatibilidades[prenda2].append(prenda1)
+
+    print(f"{prenda1} es incompatible con {prenda2}")
+
+
+def comando_n(args, tiempos, _incompatibilidades):
+    prenda, tiempo = args
+    tiempos[prenda] = int(tiempo)
+    print(f"prenda {prenda} tarda {tiempo}s")
+
+
+dic = {
+    'c': comando_c,
+    'p': comando_p,
+    'e': comando_e,
+    'n': comando_n,
+}
