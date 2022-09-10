ab4cc9bd1770ad72056792a423e79ecb6575851c
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-09-10 14:33:15 -0300

    poder ver gasto

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ def main():
     # Algoritmo
     lavados = lavados_con(tiempos, incompatibilidades)
 
-
     f = open("respuesta", "w+")
     for lavado, prendas in lavados.items():
         for prenda in prendas:
             f.write(f"{prenda} {lavado}\n")
+
+    imprimir_gasto_total(lavados, tiempos)
+
+
+def imprimir_gasto_total(lavados, tiempos):
+    suma_total = 0
+    for lavado, prendas in lavados.items():
+        suma_total += max(map(lambda x: tiempos.get(x), prendas))
+    print(suma_total)
 
 
 if __name__ == '__main__':
